48bb22c377464c1e28f292918e44688094ee69d0
kpetkowska <kingapetkowska@gmail.com>
2022-02-09 13:05:31 +0100

    new basket class

---
+++ src/main/java/Order.java
@@ -0,0 +1,44 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private List<Product> products;
+    private BigDecimal totalOrderPrice;
+
+    public Order() {
+        products = new ArrayList<>();
+    }
+
+    public Order(List<Product> products, BigDecimal totalOrderPrice) {
+        this.products = products;
+        this.totalOrderPrice = totalOrderPrice;
+    }
+
+    private void updateTotalPrice() {
+        totalOrderPrice = new BigDecimal(0);
+        for(Product product : products) {
+            totalOrderPrice = totalOrderPrice.add(product.getTotalPrice());
+        }
+    }
+
+    public void addProduct(Product productToAdd) {
+        for(Product product : products) {
+            if(product.getName().equals(productToAdd.getName())) {
+                product.addQuantity(productToAdd.getQuantity());
+                updateTotalPrice();
+                return;
+            }
+        }
+        products.add(productToAdd);
+        updateTotalPrice();
+    }
+
+    public int getTotalQuantity() {
+        int sum = 0;
+        for(Product product : products) {
+            sum += product.getQuantity();
+        }
+        return sum;
+    }
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,42 @@
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal totalPrice;
+
+    public Product(String name, BigDecimal price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, BigDecimal price, int quantity, BigDecimal totalPrice) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public void addQuantity(int quantity) {
+        this.quantity = quantity;
+        updatePrice();
+    }
+
+    private void updatePrice() {
+        totalPrice = price.multiply(new BigDecimal(quantity));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+}
